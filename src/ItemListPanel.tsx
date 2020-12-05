e013effe42ea931b0b5672287ad365e72076d435
Adrian <adrian.herd95@gmail.com>
2020-12-05 13:25:38 -0500

    ItemListPanel: skeleton file, no html defined yet

---
+++ src/ItemListPanel.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ItemListPanelState = {
+}
+
+export class ItemListPanel extends React.Component<void> {
+    constructor(props: void) {
+        super(props);
+    }
+
+    onCreate(){
+
+    }
+
+
+    render() {
+        return undefined;
+    }
+}
